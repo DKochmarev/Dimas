e0127b9b70d52b9a69cf401f519d5c57a0867b6e
DKochmarev <150533457+DKochmarev@users.noreply.github.com>
2023-12-10 15:15:00 +0500

    Update Scraping.py

--- Scraping.py
+++ Scraping.py
@@ -1,58 +1,50 @@
 import requests
 from bs4 import BeautifulSoup
-from time import sleep
 import sqlite3
+from time import sleep
 
-for count in range(1, 5):
-
-    sleep(3)
-    url = f"https://itorrents-igruha.org/newgames/page/{count}/"
+def get_urls(page):
+    url = f"https://itorrents-igruha.org/newgames/page/{page}/"
 
     response = requests.get(url)
-
     html = response.text
-
     soup = BeautifulSoup(html, "lxml")
-
     container = soup.select_one("div.articles")
-
     products = container.find_all("div", class_="article-film")
 
-    urls = []
-    for product in products:
-        url1 = product.select_one("div a")["href"]
-        if url1 != "https://itorrents-igruha.org/8456-vazhnaya-informaciya.html":
-            urls.append(url1)
+    urls = [product.select_one("div a")["href"] for product in products if product.select_one("div a")["href"] != "https://itorrents-igruha.org/8456-vazhnaya-informaciya.html"]
+    return urls
 
-    args = []
-    for url2 in urls:
-        response = requests.get(url2)
-        html = response.text
-        soup = BeautifulSoup(html, "lxml")
+def get_game_info(url):
+    response = requests.get(url)
+    html = response.text
+    soup = BeautifulSoup(html, "lxml")
 
-        name = soup.select_one("h1").text
+    name = soup.select_one("h1").text
 
-        size = soup.find("div", style="padding-left: 215px;").text
-        size_2 = size.split()
-        size_3 = 0
-        if 'диске:' in size_2:
-            size_3 = size_2[size_2.index('диске:') + 1] + size_2[size_2.index('диске:') + 2]
+    size = soup.find("div", style="padding-left: 215px;").text
+    size_2 = size.split()
+    size_3 = size_2[size_2.index('диске:') + 1] + size_2[size_2.index('диске:') + 2] if 'диске:' in size_2 else 0
 
-        if 'активация,' in size_2:
-            repack_or_activation = 'Активация'
-        else:
-            repack_or_activation = 'Репак'
+    repack_or_activation = 'Активация' if 'активация,' in size_2 else 'Репак'
 
-        views = int(soup.find("div", id="article-film-full-info").select("span")[-1].text)
+    views = int(soup.find("div", id="article-film-full-info").select("span")[-1].text)
+    likes = int(soup.select_one("a.orating_res").text[1:]) if '+' in soup.select_one("a.orating_res").text else int(soup.select_one("a.orating_res").text)
 
-        likes = soup.select_one("a.orating_res").text
-        likes_2 = int(likes[1:]) if '+' in likes else int(likes)
+    return (name, views, likes, size_3, repack_or_activation)
 
-        # args.append((name, views, likes_2, size_3, repack_or_activation))
-        args.append((name, views, likes_2, size_3, repack_or_activation))
+def move_to_database(data, db_name):
+    with sqlite3.connect(db_name) as conn:
+        cursor = conn.cursor()
+        cursor.executemany("INSERT INTO games VALUES(?,?,?,?,?)", data)
+        conn.commit()
 
-    conn = sqlite3.connect("my_database.db")
-    cursor = conn.cursor()
-    cursor.executemany("INSERT INTO games VALUES(?,?,?,?,?)", args)
-    conn.commit()
-    conn.close()
+def main():
+    for count in range(1, 5):
+        sleep(3)
+        urls = get_urls(count)
+        args = [get_game_info(url) for url in urls]
+        move_to_database(args, "qqq.db")
+
+if __name__ == "__main__":
+    main()

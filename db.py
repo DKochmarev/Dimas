d6fb49349fdd16408d2d5c5d6a983f171f69b624
DKochmarev <150533457+DKochmarev@users.noreply.github.com>
2023-12-10 15:15:32 +0500

    Update db.py

--- db.py
+++ db.py
@@ -1,14 +1,14 @@
 import sqlite3
 
-connection = sqlite3.connect("my_database.db")
-sql = '''CREATE TABLE games (name TEXT, views INTEGER, likes INTEGER, size TEXT, type TEXT)'''
-sql1 = 'SELECT * FROM games'
-cursor = connection.cursor()
-cursor.execute(sql1)
-#
-res = cursor.fetchall()
-i = 0
-for r in res:
-    print(r)
-connection.commit()
-connection.close()
+def Database():
+    with sqlite3.connect("qqq.db") as connection:
+        sql = '''CREATE TABLE games (name TEXT, views INTEGER, likes INTEGER, size TEXT, type TEXT)'''
+        sql1 = 'SELECT * FROM games'
+        cursor = connection.cursor()
+        cursor.execute(sql1)
+        results = cursor.fetchall()
+        for i, row in enumerate(results, 1):
+            print(row, i)
+
+if __name__ == "__main__":
+    Database()
